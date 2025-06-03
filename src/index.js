7e2120d3ad91301622ebc27e50c0e1644956899e
adrian161103 <adriankeomaa@gmail.com>
2025-06-03 01:05:18 -0300

    example.env y ruta vercel a cors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import path from "path";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [" https://www.pedrolamanna.com/", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
